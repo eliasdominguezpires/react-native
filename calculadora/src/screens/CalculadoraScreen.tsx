c8fadc9d6696656d70e97b599a82819b2198c818
elias.dominguezpires <elias.dominguez@konecta.com.py>
2021-07-22 16:09:36 -0400

    add calculadora

---
+++ calculadora/src/screens/CalculadoraScreen.tsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import { BotonCalc } from '../components/BotonCalc'
+import { useCalculadora } from '../hooks/useCalculadora'
+import { styles } from '../theme/appTheme'
+
+export const Calculadora = () => {
+
+    const { numeroPequeno,
+        numero,
+        limpiar,
+        positivoNegatigo,
+        deleteNumero,
+        btnOperaciones,
+        armarNumero,
+        calcular, } = useCalculadora();
+
+    return (
+        <View style={styles.calculadoraContainer}>
+                {/* {
+                    (numeroPequeno !== '0')
+                    && (<Text style={styles.resultadoPequeno}>{numeroPequeno}</Text>)
+                } */}
+            <Text style={styles.resultadoPequeno}>{numeroPequeno}</Text>
+            <Text style={styles.resultado}
+                numberOfLines={1}
+                adjustsFontSizeToFit
+            >
+                {numero}
+            </Text>
+
+            {/* Fila de Botons */}
+            <View style={styles.fila}>
+                <BotonCalc texto="C" color="#9B9B9B" accion={limpiar} />
+                <BotonCalc texto="+/-" color="#9B9B9B" accion={positivoNegatigo} />
+                <BotonCalc texto="del" color="#9B9B9B" accion={deleteNumero} />
+                <BotonCalc texto="/" color="#FF9427" accion={() => btnOperaciones('/')} />
+            </View>
+            {/* Fila Botones */}
+            <View style={styles.fila}>
+                <BotonCalc texto="7" accion={armarNumero} />
+                <BotonCalc texto="8" accion={armarNumero} />
+                <BotonCalc texto="9" accion={armarNumero} />
+                <BotonCalc texto="x" color="#FF9427" accion={() => btnOperaciones('x')} />
+            </View>
+            <View style={styles.fila}>
+                <BotonCalc texto="5" accion={armarNumero} />
+                <BotonCalc texto="5" accion={armarNumero} />
+                <BotonCalc texto="6" accion={armarNumero} />
+                <BotonCalc texto="-" color="#FF9427" accion={() => btnOperaciones('-')} />
+            </View>
+            <View style={styles.fila}>
+                <BotonCalc texto="1" accion={armarNumero} />
+                <BotonCalc texto="2" accion={armarNumero} />
+                <BotonCalc texto="3" accion={armarNumero} />
+                <BotonCalc texto="+" color="#FF9427" accion={() => btnOperaciones('+')} />
+            </View>
+            <View style={styles.fila}>
+                <BotonCalc texto="0" accion={armarNumero} ancho />
+                <BotonCalc texto="." accion={armarNumero} />
+                <BotonCalc texto="=" color="#FF9427" accion={calcular} />
+            </View>
+
+        </View>
+    )
+}
+
+{/* 2D2D2D Gris Oscuro */ }
+{/* FF9427 narana*/ }
+{/* 9B9B9B Gris claro */ }
